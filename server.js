fe35be2983fafd7f4d16c3038978382df3da3dc3
Misigno <efournier92@gmail.com>
2016-06-26 22:09:49 -0400

    finish setting up owl-carousel

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app     = express();
 var path    = require('path');
+var $       = require('jQuery');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use('jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('owl-carousel', express.static(__dirname + '/node_modules/owl-carousel-2/'));
 
 app.get('/', function(req, res) { 
   res.sendFile(path.join(__dirname + '/index.html'));
