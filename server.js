eeea022ea424f3027f66392cb9b14aa7b5392a70
Misigno <efournier92@gmail.com>
2016-06-29 18:51:05 -0400

    reboot theme

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ var path    = require('path');
 var $       = require('jQuery');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
-app.use('jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
-app.use('owl-carousel', express.static(__dirname + '/node_modules/owl-carousel-2/'));
 
 app.get('/', function(req, res) { 
   res.sendFile(path.join(__dirname + '/index.html'));
@@ -15,18 +12,3 @@ app.get('/', function(req, res) {
 app.listen(1337);
 console.log('Check out port 1337');
 
-var adminRouter = express.Router();
-
-adminRouter.get('/', function(req, res) {
-  res.send('I am the dashboard');              
-});
-
-adminRouter.get('/users', function(req, res) {
-  res.send('I show all the users!');
-});
-
-adminRouter.get('/posts', function(req, res) {
-  res.send('I show all the posts!');
-});
-
-app.use('/admin', adminRouter);
