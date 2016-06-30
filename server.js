9e91d4b42f1f97ee6c440a7818d5a8c07442bcbe
Misigno <efournier92@gmail.com>
2016-06-30 00:51:42 -0400

    require node_modules in server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path    = require('path');
 var $       = require('jquery');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/', function(req, res) { 
   res.sendFile(path.join(__dirname + '/index.html'));
