e62e8e4ff63955e0c35302d2579d5f9bf2681dd0
Misigno <efournier92@gmail.com>
2016-06-30 01:11:09 -0400

    change listening port in server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(1337);
-console.log('Check out port 1337');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
