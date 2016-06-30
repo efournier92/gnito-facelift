099ff91aaef7a906c2d25152cfe9b3e82c9a9d62
Misigno <efournier92@gmail.com>
2016-06-30 00:56:28 -0400

    remove jquery from server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app     = express();
 var path    = require('path');
-var $       = require('jquery');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/', function(req, res) { 
   res.sendFile(path.join(__dirname + '/index.html'));
